762b124ea47e2d9ad1d7c0fa1096b3c7d1c8f20a
yushan <yushan@intervest.lk>
2021-03-03 21:06:58 +0530

    feat: asynchronous action added to handle users and tweets

---
+++ src/actions/shared.js
@@ -0,0 +1,12 @@
+import { getInitialData } from '../utils/api';
+import { receiveTweets } from './tweets';
+import { receiveUsers } from './users';
+
+export default function handleInitialData() {
+    return (dispatch) => {
+        getInitialData()
+            .then(({ users, tweets }) => {
+                
+            })
+    }
+}
