4fe447deb05ea50040a510077ed9040b8da3a251
yushan <yushan@intervest.lk>
2021-03-04 11:07:47 +0530

    fix: Fixed 'receiveTweets' is defined but never used issue

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,6 @@
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 
-function receiveTweets(tweets) {
+export function receiveTweets(tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets
