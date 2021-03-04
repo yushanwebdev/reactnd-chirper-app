dc89c2d47c229541f142a3b98f02d8a99695eaf2
yushan <yushan@intervest.lk>
2021-03-04 13:05:38 +0530

    fix: Fixed middleware is not a function

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,7 @@ import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from './logger';
 
-export default applyMiddleware({
+export default applyMiddleware(
     thunk,
     logger
-});
+);
