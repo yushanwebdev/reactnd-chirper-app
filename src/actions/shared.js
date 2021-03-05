80306655e2c6dce993182ca89696ca0d89b048b4
yushan <yushan@intervest.lk>
2021-03-05 21:30:57 +0530

    feat: loading related action creators added.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api';
 import { receiveTweets } from './tweets';
 import { receiveUsers } from './users';
 import { setAuthedUser } from './authUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
     return (dispatch) => {
-        getInitialData()
+        dispatch(showLoading());
+        return getInitialData()
             .then(({ users, tweets }) => {
                 dispatch(receiveTweets(tweets));
                 dispatch(receiveUsers(users));
                 dispatch(setAuthedUser(AUTHED_ID));
+                dispatch(hideLoading());
             })
     }
 }
