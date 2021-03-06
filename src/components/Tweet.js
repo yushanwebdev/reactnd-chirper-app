dd8353745e0362e1085103f79b9e5fde9e4d0a99
yushan <yushan@intervest.lk>
2021-03-06 11:48:49 +0530

    feat: handleLike event updated to dispatch the action

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { formatDate, formatTweet } from '../utils/helpers';
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti/index';
+import { handleToggleTweet } from '../actions/tweets';
 
 class Tweet extends Component {
     toParent = (e, id) => {
@@ -11,7 +12,14 @@ class Tweet extends Component {
 
     handleLike = (e) => {
         e.preventDefault();
-        //todo: Handle like Tweet.
+        
+        const { dispatch, tweet, authedUser } = this.props;
+
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }));
     }
 
     render() {
