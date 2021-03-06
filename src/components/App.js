0f0cae5c18d03055ac8f866420d03dda4e4a6396
yushan <yushan@intervest.lk>
2021-03-06 20:15:19 +0530

    feat: routing added to the App

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
 import NewTweet from './NewTweet';
 import TweetPage from './TweetPage';
+import Nav from './Nav';
+import { Route, Router } from 'react-router';
 
 class App extends Component {
   componentDidMount() {
@@ -13,12 +15,18 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.loading === true
-        ? null 
-        : <TweetPage match={{params: '8xf0y6ziyjabvozdd253nd'}} />}
-      </div>
+      <Router>
+        <div className="container">
+          <LoadingBar />
+          {this.props.loading === true
+            ? null
+            : <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/tweet/:id" component={TweetPage} />
+                <Route path="/new" component={NewTweet} />
+              </div>}
+        </div>
+      </Router>
     )
   }
 }
