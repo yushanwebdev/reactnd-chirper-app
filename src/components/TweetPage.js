bb3300f6128bc133eb10378f446c124a8224ed6f
yushan <yushan@intervest.lk>
2021-03-06 17:55:32 +0530

    feat: TweetPage skeleton developed

---
+++ src/components/TweetPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TweetPage extends Component {
+    render() {
+        return(
+            <div>
+                Tweet Page
+            </div>
+        )
+    }
+}
+
+export default TweetPage;
