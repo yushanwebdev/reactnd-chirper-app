27dddc678c79c82f67c3d9598fe3ee69c6f02101
yushan <yushan@intervest.lk>
2021-03-06 12:26:52 +0530

    feat: Events created for NewTweet

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 class NewTweet extends Component {
+    state = {
+        text: ''
+    }
+
+    handleChange = (e) => {
+        const text = e.target.value;
+
+        this.setState(() => ({
+            text
+        }))
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const { text } = this.state;
+
+        //todo: Add tweet to the store
+
+        console.log(`New Tweet: ${text}`);
+    }
+
     render() {
         return(
             <div>
