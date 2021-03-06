f9a5b6ff28ca7e3c63f28c05e77c87dd315427ee
yushan <yushan@intervest.lk>
2021-03-06 18:17:46 +0530

    feat: TweetPage UI created

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import NewTweet from './NewTweet';
+import Tweet from './Tweet';
 
 class TweetPage extends Component {
     render() {
-        console.log(this.props);
+        const { id, replies } = this.props;
         return(
             <div>
-                Tweet Page
+                <Tweet id={id} />
+                <NewTweet id={id} />
+                {replies.length !== 0 && <h3 className="center">Replies</h3>}
+                <ul>
+                    {replies.map((replyId) => (
+                        <li key={replyId}>
+                            <Tweet id={replyId} />
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
