2289cc40a1df0e09464f5b70d029be1f14cc5910
yushan <yushan@intervest.lk>
2021-03-06 12:21:28 +0530

    feat: NewTweet skeleton developed

---
+++ src/components/NewTweet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewTweet extends Component {
+    render() {
+        return(
+            <div>
+                New Tweet
+            </div>
+        )
+    }
+}
+
+export default NewTweet;
