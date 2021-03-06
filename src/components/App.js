76b0447e5070db410db09f11f9e3c7d04651b080
yushan <yushan@intervest.lk>
2021-03-06 17:55:49 +0530

    feat: TweetPage component added

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
 import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
 
 class App extends Component {
   componentDidMount() {
@@ -16,7 +17,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
         ? null 
-        : <NewTweet />}
+        : <TweetPage />}
       </div>
     )
   }
